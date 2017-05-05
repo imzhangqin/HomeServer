f569b9df3016db68a5cbd4a047940e96374f7c69
Qin Zhang <imzhangqin@yahoo.com>
2017-05-05 15:14:01 -0500

    did migrate

--- Home/settings.py
+++ Home/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'HomeServer.urls'
+ROOT_URLCONF = 'Home.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'HomeServer.wsgi.application'
+WSGI_APPLICATION = 'Home.wsgi.application'
 
 
 # Database
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
